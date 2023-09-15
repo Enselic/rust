5ae946d5bca115d8836516880d9487e720e41f4a
Martin Nordholts <enselic@gmail.com>
2023-09-15 06:28:45 +0200

    wip  2023-09-15T06:28:45+02:00

--- tests/ui/lint/large_assignments/auxiliary/change_box.rs
+++ tests/ui/lint/large_assignments/auxiliary/change_box.rs
@@ -3,6 +3,6 @@
 
 /// This function is used to test the `large_assignments` lint.
 pub fn change_box<T>(mut x: Box<T>) -> Box<T> {
-    let contents = x.into_inner();
+    let contents = Box::<T>::into_inner(x);
     Box::new(contents)
 }
--- tests/ui/lint/large_assignments/large_move_in_lib.rs
+++ tests/ui/lint/large_assignments/large_move_in_lib.rs
@@ -1,2 +1,10 @@
 // aux-build:change_box.rs
+// build-pass
 
+#![feature(large_assignments)]
+#![move_size_limit = "1000"]
+
+fn main() {
+    let large_box = Box::new([0; 9999]);
+    let _new_large_box = change_box::change_box(large_box);
+}
