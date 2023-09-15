a637f46919f2e039a913519197afe483ef133a09
Martin Nordholts <enselic@gmail.com>
2023-09-15 06:33:09 +0200

    wip  2023-09-15T06:33:09+02:00

--- tests/ui/lint/large_assignments/auxiliary/change_box.rs
+++ tests/ui/lint/large_assignments/auxiliary/change_box.rs
@@ -1,8 +1,12 @@
 #![crate_name="change_box"]
 #![crate_type = "lib"]
 
+#![feature(large_assignments)]
+#![move_size_limit = "1000"]
+
+
 /// This function is used to test the `large_assignments` lint.
 pub fn change_box<T>(mut x: Box<T>) -> Box<T> {
-    let contents = Box::<T>::into_inner(x);
+    let contents = *x;
     Box::new(contents)
 }
--- tests/ui/lint/large_assignments/large_move_in_lib.rs
+++ tests/ui/lint/large_assignments/large_move_in_lib.rs
@@ -4,7 +4,15 @@
 #![feature(large_assignments)]
 #![move_size_limit = "1000"]
 
+extern crate change_box;
+
 fn main() {
     let large_box = Box::new([0; 9999]);
-    let _new_large_box = change_box::change_box(large_box);
+    //let _new_large_box = change_box::change_box(large_box);
+    let _new_large_box = change_box2(large_box);
+}
+
+pub fn change_box2<T>(mut x: Box<T>) -> Box<T> {
+    let contents = *x;
+    Box::new(contents)
 }
