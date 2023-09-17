57cf9e9e9a2c500ce7d60bfbfead3c156245d877
Martin Nordholts <enselic@gmail.com>
2023-09-17 19:24:14 +0200

    wip reproduces 2023-09-17T19:24:13+02:00

--- library/alloc/benches/vec.rs
+++ library/alloc/benches/vec.rs
@@ -27,46 +27,9 @@ fn bench_range_map_collect(b: &mut Bencher) {
     b.iter(|| (0..LEN).map(|_| u32::default()).collect::<Vec<_>>());
 }
 
-#[bench]
-fn bench_chain_extend_ref(b: &mut Bencher) {
-    let data = black_box([0; LEN]);
-    b.iter(|| {
-        let mut v = Vec::<u32>::with_capacity(data.len() + 1);
-        v.extend(data.iter().chain([1].iter()));
-        v
-    });
-}
 
-#[bench]
-fn bench_chain_extend_value(b: &mut Bencher) {
-    let data = black_box([0; LEN]);
-    b.iter(|| {
-        let mut v = Vec::<u32>::with_capacity(data.len() + 1);
-        v.extend(data.iter().cloned().chain(Some(1)));
-        v
-    });
-}
 
-#[bench]
-fn bench_rev_1(b: &mut Bencher) {
-    let data = black_box([0; LEN]);
-    b.iter(|| {
-        let mut v = Vec::<u32>::new();
-        v.extend(data.iter().rev());
-        v
-    });
-}
-
-#[bench]
-fn bench_rev_2(b: &mut Bencher) {
-    let data = black_box([0; LEN]);
-    b.iter(|| {
-        let mut v = Vec::<u32>::with_capacity(data.len());
-        v.extend(data.iter().rev());
-        v
-    });
-}
-
+// A
 #[bench]
 fn bench_map_regular(b: &mut Bencher) {
     let data = black_box([(0, 0); LEN]);
@@ -74,21 +37,6 @@ fn bench_map_regular(b: &mut Bencher) {
         let mut v = Vec::<u32>::new();
         v.extend(data.iter().map(|t| t.1));
         v
-    });
-}
-
-#[bench]
-fn bench_map_fast(b: &mut Bencher) {
-    let data = black_box([(0, 0); LEN]);
-    b.iter(|| {
-        let mut result: Vec<u32> = Vec::with_capacity(data.len());
-        for i in 0..data.len() {
-            unsafe {
-                *result.as_mut_ptr().add(i) = data[i].0;
-                result.set_len(i);
-            }
-        }
-        result
     });
 }
 
