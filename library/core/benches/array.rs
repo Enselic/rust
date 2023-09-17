81c8c0e7051eacb296b20c35873cf38e6c4c887b
Martin Nordholts <enselic@gmail.com>
2023-09-17 13:42:17 +0200

    wip mroe minimal 2023-09-17T13:42:16+02:00

--- library/core/benches/array.rs
+++ library/core/benches/array.rs
@@ -6,5 +6,5 @@ use test::Bencher;
 #[bench]
 fn map_256byte_8byte_256(b: &mut Bencher) {
     let arr = [0u64; 25600];
-    b.iter(|| black_box(arr).map(|_| black_box(0u64)));
+    b.iter(|| black_box(arr));
 }
