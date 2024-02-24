f135db79cbff6732014815217c87bca3b27523c7
Martin Nordholts <martin.nordholts@codetale.se>
2024-02-25 00:45:30 +0100

    wip DONE 2024-02-25T00:45:30+01:00

--- tests/run-make/sigpipe-in-child-processes/assert-sigpipe-disposition.rs
+++ tests/run-make/sigpipe-in-child-processes/assert-sigpipe-disposition.rs
@@ -2,17 +2,15 @@
 
 extern crate libc;
 
-const USAGE: &str = "Usage: assert-sigpipe-disposition SIG_IGN|SIG_DFL";
-
 // Use #[start] so we don't have a runtime that messes with SIGPIPE.
 #[start]
 fn start(argc: isize, argv: *const *const u8) -> isize {
-    assert_eq!(argc, 2, "{}", USAGE);
+    assert_eq!(argc, 2, "Must pass SIG_IGN or SIG_DFL as first arg");
     let expected =
         match unsafe { std::ffi::CStr::from_ptr(*argv.offset(1) as *const i8) }.to_str().unwrap() {
             "SIG_IGN" => libc::SIG_IGN,
             "SIG_DFL" => libc::SIG_DFL,
-            _ => panic!("{}", USAGE),
+            arg => panic!("Must pass SIG_IGN or SIG_DFL as first arg. Got: {}", arg),
         };
 
     let actual = unsafe {
