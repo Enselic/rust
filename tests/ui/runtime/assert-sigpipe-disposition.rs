26c86aefc159ad7e52a1a91a48eff6b8ab471d01
Martin Nordholts <martin.nordholts@codetale.se>
2024-02-25 00:13:50 +0100

    wip  2024-02-25T00:13:50+01:00

--- tests/ui/runtime/assert-sigpipe-disposition.rs
+++ tests/ui/runtime/assert-sigpipe-disposition.rs
@@ -8,19 +8,18 @@ const USAGE: &str = "Usage: assert-sigpipe-disposition SIG_IGN|SIG_DFL";
 #[start]
 fn start(argc: isize, argv: *const *const u8) -> isize {
     assert_eq!(argc, 2, "{}", USAGE);
-
-    let actual = unsafe {
-        let mut actual: libc::sigaction = std::mem::zeroed();
-        libc::sigaction(libc::SIGPIPE, std::ptr::null(), &mut actual);
-        actual.sa_sigaction
-    };
-
     let expected =
         match unsafe { std::ffi::CStr::from_ptr(*argv.offset(1) as *const i8) }.to_str().unwrap() {
             "SIG_IGN" => libc::SIG_IGN,
             "SIG_DFL" => libc::SIG_DFL,
             _ => panic!("{}", USAGE),
         };
+
+    let actual = unsafe {
+        let mut actual: libc::sigaction = std::mem::zeroed();
+        libc::sigaction(libc::SIGPIPE, std::ptr::null(), &mut actual);
+        actual.sa_sigaction
+    };
 
     assert_eq!(actual, expected, "actual and expected SIGPIPE disposition differs");
 
