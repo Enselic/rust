2addb7d24e348c9148665e5aa6fab7fe1fc50c55
Martin Nordholts <martin.nordholts@codetale.se>
2024-02-24 19:51:52 +0100

    wip TEMP WHY DOES THI NOT PASS 2024-02-24T19:51:52+01:00

--- tests/ui/attributes/unix_sigpipe/unix_sigpipe-in-child-processes.rs
+++ tests/ui/attributes/unix_sigpipe/unix_sigpipe-in-child-processes.rs
@@ -1,4 +1,5 @@
-//@ revisions: default sig_dfl sig_ign
+//@ revisions: default
+//@ revisions2: default sig_dfl sig_ign
 //@ run-pass
 //@ ignore-emscripten
 //@ ignore-horizon
@@ -10,9 +11,45 @@
 // attribute. Search for `unix_sigpipe_attr_specified()` in the code base to
 // learn more.
 
+#![feature(rustc_private)]
+#![allow(dead_code)]
+
 #![cfg_attr(any(sig_dfl, sig_ign), feature(unix_sigpipe))]
 
-extern crate sigpipe_utils;
+mod sigpipe_utils {
+    extern crate libc;
+
+    /// So tests don't have to bring libc in scope themselves
+    pub enum SignalHandler {
+        Ignore,
+        Default,
+    }
+
+    /// Helper to assert that [`libc::SIGPIPE`] has the expected signal handler.
+    pub fn assert_sigpipe_handler(expected_handler: SignalHandler) {
+        #[cfg(unix)]
+        #[cfg(not(any(
+            target_os = "emscripten",
+            target_os = "fuchsia",
+            target_os = "horizon",
+            target_os = "android",
+        )))]
+        {
+            let prev = unsafe { libc::signal(libc::SIGPIPE, libc::SIG_IGN) };
+
+            let expected = match expected_handler {
+                SignalHandler::Ignore => libc::SIG_IGN,
+                SignalHandler::Default => libc::SIG_DFL,
+            };
+            assert_eq!(prev, expected, "expected sigpipe value matches actual value");
+
+            // Unlikely to matter, but restore the old value anyway
+            unsafe {
+                libc::signal(libc::SIGPIPE, prev);
+            };
+        }
+    }
+}
 
 #[cfg_attr(sig_dfl, unix_sigpipe = "sig_dfl")]
 #[cfg_attr(sig_ign, unix_sigpipe = "sig_ign")]
