f43d1aa8c009622248629a43d4c3ca1324a6bd21
Martin Nordholts <martin.nordholts@codetale.se>
2025-06-10 06:22:10 +0200

    wip  2025-06-10T06:22:10+02:00

--- tests/ui/panics/panic-main.rs
+++ tests/ui/panics/panic-main.rs
@@ -1,10 +1,23 @@
-//@ revisions: zero one full
-//@ revisions: abort unwind
-//@[zero] exec-env:RUST_BACKTRACE=0
-//@[one] exec-env:RUST_BACKTRACE=1
-//@[full] exec-env:RUST_BACKTRACE=full
-//@[abort] compile-flags: -Cpanic=abort
-//@[unwind] compile-flags: -Cpanic=unwind
+//@ revisions: abort-zero abort-one abort-full panic-zero panic-one panic-full
+
+//@[abort-zero] exec-env: RUST_BACKTRACE=0
+//@[abort-zero] compile-flags: -Cpanic=abort
+
+//@[abort-one] exec-env: RUST_BACKTRACE=1
+//@[abort-one] compile-flags: -Cpanic=abort
+
+//@[abort-full] exec-env: RUST_BACKTRACE=full
+//@[abort-full] compile-flags: -Cpanic=abort
+
+//@[panic-zero] exec-env: RUST_BACKTRACE=0
+//@[panic-zero] compile-flags: -Cpanic=panic
+
+//@[panic-one] exec-env: RUST_BACKTRACE=1
+//@[panic-one] compile-flags: -Cpanic=panic
+
+//@[panic-full] exec-env: RUST_BACKTRACE=full
+//@[panic-full] compile-flags: -Cpanic=abort
+
 //@ run-fail
 //@ check-run-results
 //@ needs-subprocess
