545baa3772277f9749605b1e6f64218b49d1d939
Martin Nordholts <martin.nordholts@codetale.se>
2025-06-10 19:23:27 +0200

    wip  2025-06-10T19:23:27+02:00

--- tests/ui/panics/panic-main.rs
+++ tests/ui/panics/panic-main.rs
@@ -1,22 +1,22 @@
 //@ revisions: abort-zero abort-one abort-full unwind-zero unwind-one unwind-full
 
-//@[unwind-zero] exec-env: RUST_BACKTRACE=0
-//@[unwind-zero] compile-flags: -Cpanic=unwind
+//@[abort-zero] exec-env: RUST_BACKTRACE=0
+//@[abort-zero] compile-flags: -Cpanic=abort
+//@[abort-zero] no-prefer-dynamic
 
 //@[abort-one] exec-env: RUST_BACKTRACE=1
 //@[abort-one] compile-flags: -Cpanic=abort
+//@[abort-one] no-prefer-dynamic
 
 //@[abort-full] exec-env: RUST_BACKTRACE=full
 //@[abort-full] compile-flags: -Cpanic=abort
+//@[abort-full] no-prefer-dynamic
 
 //@[unwind-zero] exec-env: RUST_BACKTRACE=0
-//@[unwind-zero] compile-flags: -Cpanic=unwind
 
 //@[unwind-one] exec-env: RUST_BACKTRACE=1
-//@[unwind-one] compile-flags: -Cpanic=unwind
 
 //@[unwind-full] exec-env: RUST_BACKTRACE=full
-//@[unwind-full] compile-flags: -Cpanic=unwind
 
 //@ no-prefer-dynamic
 //@ run-fail
