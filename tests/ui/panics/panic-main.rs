66b2b1b8a7b1d92646a1e44643e23915ac943c87
Martin Nordholts <martin.nordholts@codetale.se>
2025-06-10 06:11:13 +0200

    wip main-panic-test 2025-06-10T06:11:13+02:00

--- tests/ui/panics/panic-main.rs
+++ tests/ui/panics/panic-main.rs
@@ -1,7 +1,21 @@
 //@ run-fail
+//@ check-run-results
 //@ error-pattern:moop
 //@ needs-subprocess
+//@ exec-env:RUST_BACKTRACE=0
+//@ exec-env:RUST_BACKTRACE=1
+//@ exec-env:RUST_BACKTRACE=full
+//@ compile-flags: -Cpanic=abort
+//@ compile-flags: -Cpanic=unwind
 
 fn main() {
     panic!("moop");
 }
+
+
+runtime-switch.rs:
+runtime-switch.run.stderr:note: Some details are omitted, run with `RUST_BACKTRACE=full` for a verbose backtrace.
+short-ice-remove-middle-frames-2.rs:
+short-ice-remove-middle-frames-2.run.stderr:note: Some details are omitted, run with `RUST_BACKTRACE=full` for a verbose backtrace.
+short-ice-remove-middle-frames.rs://@ exec-env:RUST_BACKTRACE=1
+short-ice-remove-middle-frames.run.stderr:note: Some details are omitted, run with `RUST_BACKTRACE=full` for a verbose backtrace.
