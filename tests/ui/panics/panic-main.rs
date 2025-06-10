a77669c6e5a89a8b66b594f86fda74802adab9c8
Martin Nordholts <martin.nordholts@codetale.se>
2025-06-10 06:14:08 +0200

    wip  2025-06-10T06:14:08+02:00

--- tests/ui/panics/panic-main.rs
+++ tests/ui/panics/panic-main.rs
@@ -1,21 +1,14 @@
+//@ revisions: zero one full
+//@ revisions: abort unwind
+//@[zero] exec-env:RUST_BACKTRACE=0
+//@[one] exec-env:RUST_BACKTRACE=1
+//@[full] exec-env:RUST_BACKTRACE=full
+//@[abort] compile-flags: -Cpanic=abort
+//@[unwind] compile-flags: -Cpanic=unwind
 //@ run-fail
 //@ check-run-results
-//@ error-pattern:moop
 //@ needs-subprocess
-//@ exec-env:RUST_BACKTRACE=0
-//@ exec-env:RUST_BACKTRACE=1
-//@ exec-env:RUST_BACKTRACE=full
-//@ compile-flags: -Cpanic=abort
-//@ compile-flags: -Cpanic=unwind
 
 fn main() {
     panic!("moop");
 }
-
-
-runtime-switch.rs:
-runtime-switch.run.stderr:note: Some details are omitted, run with `RUST_BACKTRACE=full` for a verbose backtrace.
-short-ice-remove-middle-frames-2.rs:
-short-ice-remove-middle-frames-2.run.stderr:note: Some details are omitted, run with `RUST_BACKTRACE=full` for a verbose backtrace.
-short-ice-remove-middle-frames.rs://@ exec-env:RUST_BACKTRACE=1
-short-ice-remove-middle-frames.run.stderr:note: Some details are omitted, run with `RUST_BACKTRACE=full` for a verbose backtrace.
