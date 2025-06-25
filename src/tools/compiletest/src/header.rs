106d8482b8b7df5d2afeca4bdf315af881d07c10
Martin Nordholts <martin.nordholts@codetale.se>
2025-06-25 07:56:40 +0200

    tests: Require `run-fail` ui tests to have an exit code (`SIGABRT` not ok)
    Normally a `run-fail` ui test shall not be terminated by a signal like
    `SIGABRT`. So begin having that as a hard requirement.
    
    Some of our current tests do terminate by a signal however. Introduce
    and use `run-crash` for those tests.
    
    And some tests behave differently on different targets. Introduce and
    use `run-fail-or-crash` for those tests. Some sanitizer tests use this
    for example, because on some platforms sanitizers will exit with code 1
    in the face of memory errors, while on others they exit with abort in
    the face of memory errors.

--- src/tools/compiletest/src/header.rs
+++ src/tools/compiletest/src/header.rs
@@ -9,7 +9,7 @@ use camino::{Utf8Path, Utf8PathBuf};
 use semver::Version;
 use tracing::*;
 
-use crate::common::{Config, Debugger, FailMode, Mode, PassMode};
+use crate::common::{Config, Debugger, FailMode, Mode, PassMode, RunFailMode};
 use crate::debuggers::{extract_cdb_version, extract_gdb_version};
 use crate::errors::ErrorKind;
 use crate::executor::{CollectedTestDesc, ShouldPanic};
@@ -655,7 +655,13 @@ impl TestProps {
             Some(FailMode::Build)
         } else if config.parse_name_directive(ln, "run-fail") {
             check_ui("run");
-            Some(FailMode::Run)
+            Some(FailMode::Run(RunFailMode::Fail))
+        } else if config.parse_name_directive(ln, "run-crash") {
+            check_ui("run");
+            Some(FailMode::Run(RunFailMode::Crash))
+        } else if config.parse_name_directive(ln, "run-fail-or-crash") {
+            check_ui("run");
+            Some(FailMode::Run(RunFailMode::FailOrCrash))
         } else {
             None
         };
