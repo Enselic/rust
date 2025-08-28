5f987eb1a6adb2061ef368d504faf0306aee8323
Martin Nordholts <martin.nordholts@codetale.se>
2025-08-28 20:11:07 +0200

    compiler: Include span of too huge enum with -Cdebuginfo=2
    We have a ui test to ensure we emit an error if we encounter too big
    enums. Before this fix, compiling the test with `-Cdebuginfo=2` would
    not include the span of the instantiation site, because the error is
    then emitted from a different code path that does not include the span.
    
    Propagate the span to the error so the test passes regardless of
    debuginfo level.

--- tests/ui/limits/huge-enum.rs
+++ tests/ui/limits/huge-enum.rs
@@ -1,9 +1,10 @@
+// FIXME(#61117): Remove revisions once x86_64-gnu-debug CI job sets rust.debuginfo-level-tests=2
+//@ revisions: debuginfo-0 debuginfo-2
 //@ build-fail
 //@ normalize-stderr: "std::option::Option<\[u32; \d+\]>" -> "TYPE"
 //@ normalize-stderr: "\[u32; \d+\]" -> "TYPE"
-
-// FIXME(#61117): Respect debuginfo-level-tests, do not force debuginfo-level=0
-//@ compile-flags: -Cdebuginfo=0
+//@[debuginfo-0] compile-flags: -Cdebuginfo=0
+//@[debuginfo-2] compile-flags: -Cdebuginfo=2
 
 #[cfg(target_pointer_width = "32")]
 type BIG = Option<[u32; (1<<29)-1]>;
