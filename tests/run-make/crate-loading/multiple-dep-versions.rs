da0475eb3bc01d687c92726a1e83787d2a920348
Martin Nordholts <martin.nordholts@codetale.se>
2025-09-06 09:58:08 +0200

    wip  2025-09-06T09:58:08+02:00

--- tests/run-make/crate-loading/multiple-dep-versions.rs
+++ tests/run-make/crate-loading/multiple-dep-versions.rs
@@ -10,14 +10,15 @@ fn main() {
     do_something(OtherType);
     do_something_type(Type);
     do_something_trait(Box::new(Type) as Box<dyn Trait2>);
-
-    // This uses `Type` from dependency-2.rs
-    into_type(MainType(42));
+    into_type(MainType(42)); // Uses `Type` from dependency-2.rs.
 }
 
 struct MainType(pub i32);
 
-// This implements for `Type` from dependency-1.rs
+// Implements `From<MainType>` for `Type` from dependency-1.rs but it is for
+// dependency-2.rs that we actually need it. It's easy to mess up in real-world
+// projects with more complicated types and dependency graphs. So we want
+// the compiler to hint of that we have multiple versions of `Type`.
 impl From<MainType> for dependency::Type {
     fn from(value: MainType) -> Self {
         dependency::Type(value.0)
