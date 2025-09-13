b39576aa9f0498a284fba187ea504b43c37cc3c2
Martin Nordholts <martin.nordholts@codetale.se>
2025-09-13 21:15:20 +0200

    trait-hint-5

--- tests/run-make/crate-loading/multiple-dep-versions.rs
+++ tests/run-make/crate-loading/multiple-dep-versions.rs
@@ -4,12 +4,12 @@ use dep_2_reexport::{OtherType, Trait2, Type, into_type};
 use dependency::{Trait, do_something, do_something_trait, do_something_type};
 
 fn main() {
-    do_something(Type);
-    Type.foo();
-    Type::bar();
-    do_something(OtherType);
-    do_something_type(Type);
-    do_something_trait(Box::new(Type) as Box<dyn Trait2>);
+    //do_something(Type);
+    //Type.foo();
+    //Type::bar();
+    //do_something(OtherType);
+    //do_something_type(Type);
+    //do_something_trait(Box::new(Type) as Box<dyn Trait2>);
     into_type(MainType(42));
 }
 
