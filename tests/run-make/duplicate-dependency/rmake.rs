da33697b6e66230adcd0fbc54690f04ac69b4874
Martin Nordholts <martin.nordholts@codetale.se>
2025-09-20 21:41:52 +0200

    wip  2025-09-20T21:41:52+02:00

--- tests/run-make/duplicate-dependency/rmake.rs
+++ tests/run-make/duplicate-dependency/rmake.rs
@@ -13,7 +13,7 @@ fn main() {
         .input("main.rs")
         .edition("2018")
         .extern_("foo", rust_lib_name("foo-v1"))
-        .extern_("re-export-foo", rust_lib_name("re-export-foo"))
+        .extern_("re_export_foo", rust_lib_name("re_export_foo"))
         .library_search_path(cwd())
         .ui_testing()
         .remap_path_prefix(cwd(), "/nordholts")
