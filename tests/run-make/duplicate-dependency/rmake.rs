305f7ab9a79d32dddee38446101a9c4b2e5899ee
Martin Nordholts <martin.nordholts@codetale.se>
2025-09-20 21:41:17 +0200

    wip  2025-09-20T21:41:17+02:00

--- tests/run-make/duplicate-dependency/rmake.rs
+++ tests/run-make/duplicate-dependency/rmake.rs
@@ -5,14 +5,16 @@
 use run_make_support::{cwd, diff, rust_lib_name, rustc};
 
 fn main() {
-    rustc().input("foo-v1.rs").extra_filename("-v1").metadata("-v1").run();
-    rustc().input("foo-v2.rs").extra_filename("-v2").metadata("-v2").run();
-    rustc().input("re-export-foo.rs").extern_("foo", rust_lib_name("foo-v2")).run();
+    rustc().input("foo-v1.rs").crate_type("rlib").crate_name("foo").extra_filename("-v1").metadata("-v1").run();
+    rustc().input("foo-v2.rs").crate_type("rlib").crate_name("foo").extra_filename("-v2").metadata("-v2").run();
+    rustc().input("re-export-foo.rs").crate_type("rlib").edition("2018").extern_("foo", rust_lib_name("foo-v2")).run();
 
     let out = rustc()
         .input("main.rs")
+        .edition("2018")
         .extern_("foo", rust_lib_name("foo-v1"))
         .extern_("re-export-foo", rust_lib_name("re-export-foo"))
+        .library_search_path(cwd())
         .ui_testing()
         .remap_path_prefix(cwd(), "/nordholts")
         .run_fail()
@@ -20,6 +22,6 @@ fn main() {
 
     diff()
         .expected_file("main.stderr")
-        .actual_text("(rustc)", &lines.join("\n"))
+        .actual_text("(rustc)", &out)
         .run();
 }
