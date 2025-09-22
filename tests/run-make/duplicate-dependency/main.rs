b023c880272f35c7511890730d2bc5ff9bf75809
Martin Nordholts <martin.nordholts@codetale.se>
2025-09-22 07:14:23 +0200

    wip  2025-09-22T07:14:23+02:00

--- tests/run-make/duplicate-dependency/main.rs
+++ tests/run-make/duplicate-dependency/main.rs
@@ -1,14 +1,15 @@
-use foo::Foo;
-use re_export_foo::into_foo;
-
 struct Bar;
 
-impl From<Bar> for Foo {
+impl From<Bar> for foo::Foo {
     fn from(_: Bar) -> Self {
-        Foo
+        foo::Foo
     }
 }
 
 fn main() {
-    into_foo(Bar);
+    // The user might wrongly expect this to work since From<Bar> for Foo
+    // implies Into<Foo> for Bar. What the user missed is that different
+    // versions of Foo exist in the dependency graph, and the impl is for the
+    // wrong version.
+    re_export_foo::into_foo(Bar);
 }
