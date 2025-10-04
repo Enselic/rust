6af34cbc75cd65f19bf3fe2fe367015384e18aa2
Martin Nordholts <martin.nordholts@codetale.se>
2025-10-04 11:13:40 +0200

    wip  2025-10-04T11:13:40+02:00

--- tests/ui/panics/panic-backtrace-snapshot-x86_64-linux.rs
+++ tests/ui/panics/panic-backtrace-snapshot-x86_64-linux.rs
@@ -1,14 +1,20 @@
-//@ revisions: force-unwind-tables-no_debuginfo-zero_strip-symbols
-//@[force-unwind-tables-no_debuginfo-zero_strip-symbols] compile-flags: -Cforce-unwind-tables=no -Cdebuginfo=0 -Cstrip=symbols
+//@ revisions: panic-unwind_force-unwind-tables-no_debuginfo-zero_strip-symbols
+//@[force-unwind-tables-no_debuginfo-zero_strip-symbols] compile-flags: -Cpanic=unwind -Cforce-unwind-tables=no -Cdebuginfo=0 -Cstrip=symbols
 
-//@ revisions: force-unwind-tables-no_debuginfo-zero_strip-none
-//@[force-unwind-tables-no_debuginfo-zero_strip-none] compile-flags: -Cforce-unwind-tables=no -Cdebuginfo=0 -Cstrip=none
+//@ revisions: panic-unwind_force-unwind-tables-no_debuginfo-zero_strip-none
+//@[force-unwind-tables-no_debuginfo-zero_strip-none] compile-flags: -Cpanic=unwind -Cforce-unwind-tables=no -Cdebuginfo=0 -Cstrip=none
+
+//@ revisions: panic-abort_force-unwind-tables-no_debuginfo-zero_strip-symbols
+//@[force-unwind-tables-no_debuginfo-zero_strip-symbols] compile-flags: -Cpanic=abort -Cforce-unwind-tables=no -Cdebuginfo=0 -Cstrip=symbols
+
+//@ revisions: panic-abort_force-unwind-tables-no_debuginfo-zero_strip-none
+//@[force-unwind-tables-no_debuginfo-zero_strip-none] compile-flags: -Cpanic=abort -Cforce-unwind-tables=no -Cdebuginfo=0 -Cstrip=none
 
 //@ only-x86_64
 //@ only-linux
 //@ check-run-results
 //@ run-fail
-//@ exec-env:RUST_BACKTRACE=1
+//@ exec-env:RUST_BACKTRACE=short
 
 fn main() {
     panic!("the panic payload");
