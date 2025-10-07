f9c5fda99b88bf895c4f634ec91a4db69e0d0bb6
Martin Nordholts <martin.nordholts@codetale.se>
2025-10-07 06:41:09 +0200

    tests: basic-[debugger-]stepping.rs: Disable SingleUseConsts MIR pass TODO: Rewrite commit msg
    To make more of the test pass. It should not be necessary to disable
    this pass, but by doing it we can avoid further regressions while we
    figure out how to solve this use case properly.
    
    The last two `FIXME(33013)` didn't go away from this change, only the
    first six. But that can be investigated separately.

--- tests/debuginfo/basic-stepping.rs
+++ tests/debuginfo/basic-stepping.rs
@@ -8,6 +8,10 @@
 //@ compile-flags: -g
 //@ ignore-backends: gcc
 
+// FIXME: It should not be necessary to disable SingleUseConsts. Revisions should not be needed in this test.
+//@ revisions default-mir-passes no-SingleUseConsts-mir-pass
+//@[no-SingleUseConsts-mir-pass] compile-flags: -Zmir-enable-passes=-SingleUseConsts
+
 //@ gdb-command: run
 // FIXME(#97083): Should we be able to break on initialization of zero-sized types?
 // FIXME(#97083): Right now the first breakable line is:
@@ -15,12 +19,12 @@
 //@ gdb-command: next
 //@ gdb-check:   let d = c = 99;
 //@ gdb-command: next
-// FIXME(#33013): gdb-check:   let e = "hi bob";
-// FIXME(#33013): gdb-command: next
-// FIXME(#33013): gdb-check:   let f = b"hi bob";
-// FIXME(#33013): gdb-command: next
-// FIXME(#33013): gdb-check:   let g = b'9';
-// FIXME(#33013): gdb-command: next
+//@[no-SingleUseConsts-mir-pass]: gdb-check:   let e = "hi bob";
+//@[no-SingleUseConsts-mir-pass]: gdb-command: next
+//@[no-SingleUseConsts-mir-pass]: gdb-check:   let f = b"hi bob";
+//@[no-SingleUseConsts-mir-pass]: gdb-command: next
+//@[no-SingleUseConsts-mir-pass]: gdb-check:   let g = b'9';
+//@[no-SingleUseConsts-mir-pass]: gdb-command: next
 //@ gdb-check:   let h = ["whatever"; 8];
 //@ gdb-command: next
 //@ gdb-check:   let i = [1,2,3,4];
