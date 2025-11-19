4bd7bd0f1b60ee8b279e650ac744bc9c54669ebb
Martin Nordholts <martin.nordholts@codetale.se>
2025-11-19 20:40:56 +0100

    wip  2025-11-19T20:40:56+01:00

--- compiler/rustc_borrowck/src/diagnostics/mutability_errors.rs
+++ compiler/rustc_borrowck/src/diagnostics/mutability_errors.rs
@@ -1199,8 +1199,9 @@ impl<'infcx, 'tcx> MirBorrowckCtxt<'_, 'infcx, 'tcx> {
             return;
         }
 
-        let source = self.body.source;
-        if let InstanceKind::Item(def_id) = source.instance
+        // If `local` is a closure arg, and the type of the arg is not under
+        // local control, do not suggest to change its type.
+        if self.body.local_kind(local) == LocalKind::Arg && let InstanceKind::Item(def_id) = self.body.source.instance
             && let Some(Node::Expr(hir::Expr { hir_id, kind, .. })) =
                 self.infcx.tcx.hir_get_if_local(def_id)
             && let ExprKind::Closure(hir::Closure { kind: hir::ClosureKind::Closure, .. }) = kind
--- tests/ui/borrowck/option-inspect-mutation.rs
+++ tests/ui/borrowck/option-inspect-mutation.rs
@@ -1,11 +1,11 @@
-#[derive(Debug)]
-struct A {
-    a: u32,
+struct Struct {
+    field: u32,
 }
 
 fn main() {
-    let mut opt = Some(A { a: 123 });
-    let ref_mut_opt = opt.as_mut().inspect(|a| {
-        a.a += 123; //~ ERROR cannot assign to `a.a`, which is behind a `&` reference
+    let mut some_struct = Some(Struct { field: 42 });
+    some_struct.as_mut().inspect(|some_struct| {
+        some_struct.field *= 10; //~ ERROR cannot assign to `some_struct.field`, which is behind a `&` reference
+        // Users can't change type of `some_struct` param, so above error must not suggest it.
     });
 }
