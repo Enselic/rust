7f0b0fa639b79f5e3f39951861ad19eca871ccc9
Martin Nordholts <martin.nordholts@codetale.se>
2025-12-22 23:30:00 +0100

    wip  2025-12-22T23:30:00+01:00

--- tests/assembly-llvm/some-non-zero-from-atomic-optimization.rs
+++ tests/assembly-llvm/some-non-zero-from-atomic-optimization.rs
@@ -1,12 +1,9 @@
 //! Regression test for <https://github.com/rust-lang/rust/issues/60044>.
 
-//@ assembly-output: emit-asm
-//@ only-x86_64
-
 // We want to check that the None case is optimized away
 //@ compile-flags: -O
 
-// Simplify the generated assembly
+// Simplify the emitted code
 //@ compile-flags: -Cforce-unwind-tables=no
 
 #![crate_type = "lib"]
@@ -19,7 +16,6 @@ pub static X: AtomicUsize = AtomicUsize::new(1);
 
 /// This function function shall look like this:
 /// ```llvm
-/// ; Function Attrs: mustprogress nofree norecurse nounwind nonlazybind willreturn memory(readwrite, argmem: none, inaccessiblemem: write)
 /// define noundef range(i64 1, 0) i64 @some_non_zero_from_atomic_get() unnamed_addr #0 {
 /// start:
 ///   %0 = load atomic i64, ptr @_ZN38some_non_zero_from_atomic_optimization1X17h41fcdb7c72ef9763E monotonic, align 8
@@ -27,6 +23,7 @@ pub static X: AtomicUsize = AtomicUsize::new(1);
 ///   tail call void @llvm.assume(i1 %1)
 ///   ret i64 %0
 /// }
+/// attributes #0 = { mustprogress nofree norecurse nounwind nonlazybind willreturn memory(readwrite, argmem: none, inaccessiblemem: write) "probe-stack"="inline-asm" "target-cpu"="x86-64" }
 /// ```
 // CHECK-LABEL: define noundef range(i64 1, 0) i64 @some_non_zero_from_atomic_get() unnamed_addr #[[#ATTRIBUTE_GROUP:]] {
 // CHECK-NEXT:  start:
@@ -35,23 +32,20 @@ pub static X: AtomicUsize = AtomicUsize::new(1);
 // CHECK-NEXT:    tail call void @llvm.assume(i1 %1)
 // CHECK-NEXT:    ret i64 %0
 // CHECK-NEXT:  }
+// CHECK:       attributes #[[#ATTRIBUTE_GROUP]] =
+// CHECK-SAME:  mustprogress
+// CHECK-SAME:  nofree
+// CHECK-SAME:  norecurse
+// CHECK-SAME:  willreturn
+// CHECK-SAME:  nounwind
 #[no_mangle]
 pub unsafe fn some_non_zero_from_atomic_get() -> Option<NonZeroUsize> {
     let x = X.load(Relaxed);
     Some(NonZeroUsize::new_unchecked(x))
 }
 
-/// This function shall be identical to the above, which mean that for LLVM 21
-/// we should see
-/// ```
-/// some_non_zero_from_atomic_get2 = some_non_zero_from_atomic_get
-/// ```
-/// and for LLVM 20 we should see
-/// ```
-/// .set some_non_zero_from_atomic_get2, some_non_zero_from_atomic_get
-/// ```
-/// so use a regex to match either one.
-// CHECK-DAG: {{\.set some_non_zero_from_atomic_get2, some_non_zero_from_atomic_get|some_non_zero_from_atomic_get2 = some_non_zero_from_atomic_get}}
+/// This function shall be identical to the above, which means:
+// CHECK-DAG: @some_non_zero_from_atomic_get2 = unnamed_addr alias i64 (), ptr @some_non_zero_from_atomic_get
 #[no_mangle]
 pub unsafe fn some_non_zero_from_atomic_get2() -> usize {
     match some_non_zero_from_atomic_get() {
