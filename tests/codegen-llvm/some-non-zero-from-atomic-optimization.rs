6d1c2019aa26f64593327f1aaefcbc256452ac71
Martin Nordholts <martin.nordholts@codetale.se>
2025-12-23 00:06:57 +0100

    wip  2025-12-23T00:06:57+01:00

--- tests/codegen-llvm/some-non-zero-from-atomic-optimization.rs
+++ tests/codegen-llvm/some-non-zero-from-atomic-optimization.rs
@@ -23,21 +23,23 @@ pub static X: AtomicUsize = AtomicUsize::new(1);
 ///   tail call void @llvm.assume(i1 %1)
 ///   ret i64 %0
 /// }
+/// ; ...
 /// attributes #0 = { mustprogress nofree norecurse nounwind nonlazybind willreturn memory(readwrite, argmem: none, inaccessiblemem: write) "probe-stack"="inline-asm" "target-cpu"="x86-64" }
 /// ```
-// CHECK-LABEL: define noundef range(i64 1, 0) i64 @some_non_zero_from_atomic_get() unnamed_addr #[[#ATTRIBUTE_GROUP:]] {
-// CHECK-NEXT:  start:
-// CHECK-NEXT:    %0 = load atomic i64, ptr @{{[_a-zA-Z0-9]+}} monotonic, align 8
-// CHECK-NEXT:    %1 = icmp ne i64 %0, 0
-// CHECK-NEXT:    tail call void @llvm.assume(i1 %1)
-// CHECK-NEXT:    ret i64 %0
-// CHECK-NEXT:  }
-// CHECK:       attributes #[[#ATTRIBUTE_GROUP]] =
-// CHECK-SAME:  mustprogress
-// CHECK-SAME:  nofree
-// CHECK-SAME:  norecurse
-// CHECK-SAME:  nounwind
-// CHECK-SAME:  willreturn
+// CHECK-LABEL: define noundef range(i64 1, 0) i64 @some_non_zero_from_atomic_get() unnamed_addr
+// CHECK-SAME: #[[#ATTRIBUTE_GROUP:]] {
+// CHECK-NEXT: start:
+// CHECK-NEXT:   %0 = load atomic i64, ptr @{{[_a-zA-Z0-9]+}} monotonic, align 8
+// CHECK-NEXT:   %1 = icmp ne i64 %0, 0
+// CHECK-NEXT:   tail call void @llvm.assume(i1 %1)
+// CHECK-NEXT:   ret i64 %0
+// CHECK-NEXT: }
+// CHECK: attributes #[[#ATTRIBUTE_GROUP]] =
+// CHECK-SAME: mustprogress
+// CHECK-SAME: nofree
+// CHECK-SAME: norecurse
+// CHECK-SAME: nounwind
+// CHECK-SAME: willreturn
 #[no_mangle]
 pub unsafe fn some_non_zero_from_atomic_get() -> Option<NonZeroUsize> {
     let x = X.load(Relaxed);
@@ -45,7 +47,7 @@ pub unsafe fn some_non_zero_from_atomic_get() -> Option<NonZeroUsize> {
 }
 
 /// This function shall be identical to the above, which means:
-// CHECK-DAG: @some_non_zero_from_atomic_get2 = unnamed_addr alias i64 (), ptr @some_non_zero_from_atomic_get
+// CHECK-LABEL: @some_non_zero_from_atomic_get2 = unnamed_addr alias i64 (), ptr @some_non_zero_from_atomic_get
 #[no_mangle]
 pub unsafe fn some_non_zero_from_atomic_get2() -> usize {
     match some_non_zero_from_atomic_get() {
