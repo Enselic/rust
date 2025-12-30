1481521ff5b00e918388046d2a4d70e50eca22ed
Martin Nordholts <martin.nordholts@codetale.se>
2025-12-30 14:58:27 +0100

    wip  2025-12-30T14:58:27+01:00

--- library/std/src/io/mod.rs
+++ library/std/src/io/mod.rs
@@ -2249,8 +2249,8 @@ pub enum OnBrokenPipe {
 
 /// How to change SIGPIPE disposition before `fn main()`. `None` means
 /// "inherit from parent process".
-//#[eii]
-#[unstable(feature = "on_broken_pipe", issue = "97889")] // TODO: create new issue
+#[eii(on_broken_pipe)]
+// #[unstable(feature = "on_broken_pipe", issue = "97889")] // TODO: create new issue
 pub fn on_broken_pipe() -> OnBrokenPipe {
     OnBrokenPipe::Default
 }
