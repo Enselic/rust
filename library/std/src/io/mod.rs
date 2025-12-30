c28a0a24050b773d64d3bedbc5b380b8a864b437
Martin Nordholts <martin.nordholts@codetale.se>
2025-12-30 13:54:06 +0100

    wip  2025-12-30T13:54:06+01:00

--- library/std/src/io/mod.rs
+++ library/std/src/io/mod.rs
@@ -2220,36 +2220,38 @@ pub(crate) fn stream_len_default<T: Seek + ?Sized>(self_: &mut T) -> Result<u64>
 /// [`ErrorKind::BrokenPipe`]. Currently only relevant to the `unix` family of
 /// operating systems.
 #[unstable(feature = "on_broken_pipe", issue = "97889")] // TODO: create new issue
-#[derive(Debug, PartialEq, Eq, Default)]
 #[non_exhaustive] // We want to be able to add more variants later.
 pub enum OnBrokenPipe {
     /// Set `SIGPIPE` to `SIG_IGN` so that pipe I/O problems are reported as
     /// [`ErrorKind::BrokenPipe`] errors. Reset `SIGPIPE` to `SIG_DFL` before
     /// child `exec()`. This has been the default behavior since Rust 1.0.
-    #[default]
+    #[unstable(feature = "on_broken_pipe", issue = "97889")] // TODO: create new issue
     Default,
     /// Set `SIGPIPE` to `SIG_IGN` so that pipe I/O problems kills the process.
     /// Don't touch `SIGPIPE` before child `exec()`.
     ///
     /// This is mainly useful when you want programs to terminate when their
     /// output is piped to short-lived programs like `head`.
+    #[unstable(feature = "on_broken_pipe", issue = "97889")] // TODO: create new issue
     Kill,
     /// Set `SIGPIPE` to `SIG_DFL` so that pipe I/O problems are reported as
     /// [`ErrorKind::BrokenPipe`] errors. Don't touch `SIGPIPE` before child
     /// `exec()`.
+    #[unstable(feature = "on_broken_pipe", issue = "97889")] // TODO: create new issue
     Error,
     /// Never touch `SIGPIPE`, including before child `exec()`.
     /// `SIGPIPE` disposition is always inherited from the parent process.
     /// This typically means that programs behave as with [`Self::Kill`].
+    #[unstable(feature = "on_broken_pipe", issue = "97889")] // TODO: create new issue
     Inherit,
 }
 
 /// How to change SIGPIPE disposition before `fn main()`. `None` means
 /// "inherit from parent process".
 #[eii]
-//#[unstable(feature = "on_broken_pipe", issue = "97889")] // TODO: create new issue
-fn on_broken_pipe() -> OnBrokenPipe {
-    Default::default()
+#[unstable(feature = "on_broken_pipe", issue = "97889")] // TODO: create new issue
+pub fn on_broken_pipe() -> OnBrokenPipe {
+    OnBrokenPipe::Default
 }
 
 /// Enumeration of possible methods to seek within an I/O object.
