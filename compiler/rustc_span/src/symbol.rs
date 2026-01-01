11ffb84684a546a3b759a3ebb43c9cdbbfd42b90
Martin Nordholts <martin.nordholts@codetale.se>
2026-01-01 11:36:41 +0100

    make std eii work

--- compiler/rustc_span/src/symbol.rs
+++ compiler/rustc_span/src/symbol.rs
@@ -2412,6 +2412,7 @@ symbols! {
                           from crates.io via `Cargo.toml` instead?",
         untagged_unions,
         unused_imports,
+        unused_macros,
         unwind,
         unwind_attributes,
         unwind_safe_trait,
