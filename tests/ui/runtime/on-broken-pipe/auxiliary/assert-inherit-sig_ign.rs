a4bc234ba2014a1ed9de1edb7c44f83ca8c134ba
Martin Nordholts <martin.nordholts@codetale.se>
2026-01-05 10:36:57 +0100

    tests/ui/runtime/on-broken-pipe/inherit.rs

--- tests/ui/runtime/on-broken-pipe/auxiliary/assert-inherit-sig_ign.rs
+++ tests/ui/runtime/on-broken-pipe/auxiliary/assert-inherit-sig_ign.rs
@@ -1,5 +1,11 @@
 //@ aux-crate: sigpipe_utils=sigpipe-utils.rs
-//@ compile-flags: -Zon-broken-pipe=inherit
+
+#![feature(on_broken_pipe)]
+
+#[std::io::on_broken_pipe]
+fn inherit_on_broken_pipe() -> std::io::OnBrokenPipe {
+    std::io::OnBrokenPipe::Inherit
+}
 
 fn main() {
     sigpipe_utils::assert_sigpipe_handler(sigpipe_utils::SignalHandler::Ignore);
