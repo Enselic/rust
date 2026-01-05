e65ce7131c28371ec591f61bdb6101690b918edc
Martin Nordholts <martin.nordholts@codetale.se>
2026-01-05 17:36:45 +0100

    wip this seems to work 2026-01-05T17:36:45+01:00

--- tests/ui/runtime/on-broken-pipe/auxiliary/assert-inherit-sig_dfl.rs
+++ tests/ui/runtime/on-broken-pipe/auxiliary/assert-inherit-sig_dfl.rs
@@ -2,12 +2,24 @@
 
 #![feature(on_broken_pipe)]
 #![feature(extern_item_impls)]
+#![feature(rustc_private)]
+
+extern crate libc;
 
 #[std::io::on_broken_pipe]
 fn inherit_on_broken_pipe() -> std::io::OnBrokenPipe {
-    std::io::OnBrokenPipe::Inherit
+    std::io::OnBrokenPipe::Kill
 }
 
 fn main() {
-    sigpipe_utils::assert_sigpipe_handler(sigpipe_utils::SignalHandler::Default);
+    //sigpipe_utils::assert_sigpipe_handler(sigpipe_utils::SignalHandler::Default);
+
+    // TODO Remove this block
+    let actual = unsafe {
+        let mut actual: libc::sigaction = std::mem::zeroed();
+        libc::sigaction(libc::SIGPIPE, std::ptr::null(), &mut actual);
+        actual.sa_sigaction
+    };
+    let expected = libc::SIG_DFL;
+    assert_eq!(actual, expected, "actual {actual} and expected {expected} SIGPIPE disposition differs");
 }
