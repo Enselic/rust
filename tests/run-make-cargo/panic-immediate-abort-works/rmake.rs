737ea47d01da5714853a82af699cdaef1c7ba0ba
Martin Nordholts <martin.nordholts@codetale.se>
2026-01-09 18:49:24 +0100

    wip enable tests/run-make-cargo/panic-immediate-abort-works/rmake.rs 2026-01-09T18:49:23+01:00

--- tests/run-make-cargo/panic-immediate-abort-works/rmake.rs
+++ tests/run-make-cargo/panic-immediate-abort-works/rmake.rs
@@ -11,9 +11,6 @@
 //@ needs-target-std
 //@ ignore-cross-compile
 
-//@ ignore-64bit FIXME: Make the test work!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-//@ ignore-32bit FIXME: Make the test work!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
 #![deny(warnings)]
 
 use run_make_support::{cargo, path, target};
