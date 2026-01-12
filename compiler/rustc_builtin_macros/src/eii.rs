1a41d5080889ddc1c44a9e6f2466a861f4001c5e
Martin Nordholts <martin.nordholts@codetale.se>
2026-01-12 06:53:19 +0100

    compiler: Make Externally Implementable Items (eii) macro "semiopaque"
    To avoid large amounts of `missing stability attribute` errors as
    described in 146993.

--- compiler/rustc_builtin_macros/src/eii.rs
+++ compiler/rustc_builtin_macros/src/eii.rs
@@ -366,6 +366,14 @@ fn generate_attribute_macro_to_implement(
     // errors for eii's in std.
     macro_attrs.extend_from_slice(attrs_from_decl);
 
+    // Needed to avoid lots of unrelated "missing stability attribute" errors on
+    // unrelated items. See #146993.
+    macro_attrs.push(ecx.attr_name_value_str(
+        sym::rustc_macro_transparency,
+        sym::semiopaque,
+        span,
+    ));
+
     // #[builtin_macro(eii_shared_macro)]
     macro_attrs.push(ecx.attr_nested_word(sym::rustc_builtin_macro, sym::eii_shared_macro, span));
 
