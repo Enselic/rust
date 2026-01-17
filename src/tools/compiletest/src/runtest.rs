2101f39afe05467d1bfb02ab733f499a951323fa
Martin Nordholts <martin.nordholts@codetale.se>
2026-01-17 15:43:39 +0100

    wip  2026-01-17T15:43:39+01:00

--- src/tools/compiletest/src/runtest.rs
+++ src/tools/compiletest/src/runtest.rs
@@ -1295,9 +1295,9 @@ impl<'test> TestCx<'test> {
             }
         };
 
-        for AuxCrate { name, path } in &self.props.aux.crates {
+        for AuxCrate { extern_options, name, path } in &self.props.aux.crates {
             let aux_type = self.build_auxiliary(&path, &aux_dir, None);
-            add_extern(rustc, None /* options TODO constant */, name, path, aux_type);
+            add_extern(rustc, extern_options.as_deref(), name, path, aux_type);
         }
 
         for proc_macro in &self.props.aux.proc_macros {
