f0bc950c5d707bdaf1eef60627d5e4ae9864e415
Martin Nordholts <martin.nordholts@codetale.se>
2026-01-26 15:59:08 +0100

    wip dylib-only-for-prefer-dynamic-2 2026-01-26T15:59:08+01:00

---
+++ tests/ui/compiletest-self-test/auxiliary/no_prefer_dynamic_print_hello.rs
@@ -0,0 +1,6 @@
+//@ no-prefer-dynamic
+#![crate_type = "rlib"]
+
+pub fn no_prefer_dynamic_print_hello() {
+    println!("Hello from no_prefer_dynamic_print_hello!");
+}
---
+++ tests/ui/compiletest-self-test/no-prefer-dynamic-means-no-so.rs
@@ -0,0 +1,6 @@
+//@ no-prefer-dynamic
+//@ aux-crate: no_prefer_dynamic_print_hello=no_prefer_dynamic_print_hello.rs
+
+fn main() {
+    no_prefer_dynamic_print_hello::no_prefer_dynamic_print_hello();
+}
