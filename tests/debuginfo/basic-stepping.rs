df7d5567e278e657ba4483745ca2f9e977fd0d0b
Martin Nordholts <martin.nordholts@codetale.se>
2026-03-11 06:54:38 +0100

    wip  2026-03-11T06:54:38+01:00

--- tests/debuginfo/basic-stepping.rs
+++ tests/debuginfo/basic-stepping.rs
@@ -10,6 +10,8 @@
 // Debugger tests need debuginfo
 //@ compile-flags: -g
 
+// The `SingleUseConsts` MIR pass _improves_ compile times, so we want to run it even with -O0.
+// For variables that 
 // FIXME(#128945): SingleUseConsts shouldn't need to be disabled.
 //@ revisions: default-mir-passes no-SingleUseConsts-mir-pass
 //@ [no-SingleUseConsts-mir-pass] compile-flags: -Zmir-enable-passes=-SingleUseConsts
@@ -38,6 +40,43 @@
 //@ gdb-check:   let l = &i[k];
 //@ gdb-command: next
 //@ gdb-check:   let m: *const() = &a;
+
+// === LLDB TESTS ==================================================================================
+
+//@ lldb-command:run
+// FIXME(#97083): Should we be able to break on initialization of zero-sized types?
+// FIXME(#97083): Right now the first breakable line is:
+//@ lldb-check:[...]let mut c = 27;[...]
+//@ lldb-command:next
+//@ lldb-command:frame select
+//@ lldb-check:[...]let d = c = 99;[...]
+//@ lldb-command:next
+//@ [no-SingleUseConsts-mir-pass] lldb-command:frame select
+//@ [no-SingleUseConsts-mir-pass] lldb-check:[...]let e = "hi bob";[...]
+//@ [no-SingleUseConsts-mir-pass] lldb-command:next
+//@ [no-SingleUseConsts-mir-pass] lldb-command:frame select
+//@ [no-SingleUseConsts-mir-pass] lldb-check:[...]let f = b"hi bob";[...]
+//@ [no-SingleUseConsts-mir-pass] lldb-command:next
+//@ [no-SingleUseConsts-mir-pass] lldb-command:frame select
+//@ [no-SingleUseConsts-mir-pass] lldb-check:[...]let g = b'9';[...]
+//@ [no-SingleUseConsts-mir-pass] lldb-command:next
+//@ lldb-command:frame select
+//@ lldb-check:[...]let h = ["whatever"; 8];[...]
+//@ lldb-command:next
+//@ lldb-command:frame select
+//@ lldb-check:[...]let i = [1,2,3,4];[...]
+//@ lldb-command:next
+//@ lldb-command:frame select
+//@ lldb-check:[...]let j = (23, "hi");[...]
+//@ lldb-command:next
+//@ lldb-command:frame select
+//@ lldb-check:[...]let k = 2..3;[...]
+//@ lldb-command:next
+//@ lldb-command:frame select
+//@ lldb-check:[...]let l = &i[k];[...]
+//@ lldb-command:next
+//@ lldb-command:frame select
+//@ lldb-check:[...]let m: *const() = &a;[...]
 
 fn main () {
     let a = (); // #break
