295adf8ca898965bf217b7ccf8115337d7421051
Martin Nordholts <martin.nordholts@codetale.se>
2026-03-13 23:37:03 +0100

    wip  2026-03-13T23:37:03+01:00

--- tests/debuginfo/basic-stepping.rs
+++ tests/debuginfo/basic-stepping.rs
@@ -93,8 +93,8 @@
 // Enable line-based debugging and print lines after stepping.
 // See https://learn.microsoft.com/en-us/windows-hardware/drivers/debuggercmds/-lines--toggle-source-line-support-
 // and https://learn.microsoft.com/en-us/windows-hardware/drivers/debuggercmds/l---l---set-source-options-.
-//@ cdb-command: .lines -e
-// cdb-command: l+s
+// cdb-command: .lines -e
+//@ cdb-command: l+s
 // cdb-command: l+t
 
 //@ cdb-command: g
