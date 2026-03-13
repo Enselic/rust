1a6d0fc8d7fa08f8b1841cc61d8d6a3b2864a060
Martin Nordholts <martin.nordholts@codetale.se>
2026-03-13 23:38:03 +0100

    wip  2026-03-13T23:38:03+01:00

--- tests/debuginfo/basic-stepping.rs
+++ tests/debuginfo/basic-stepping.rs
@@ -94,7 +94,7 @@
 // See https://learn.microsoft.com/en-us/windows-hardware/drivers/debuggercmds/-lines--toggle-source-line-support-
 // and https://learn.microsoft.com/en-us/windows-hardware/drivers/debuggercmds/l---l---set-source-options-.
 // cdb-command: .lines -e
-// cdb-command: l+s
+//@ cdb-command: l+s
 //@ cdb-command: l+t
 
 //@ cdb-command: g
