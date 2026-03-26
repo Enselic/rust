40aaa9979a786a70d5812ace426b3f041b5758e4
Martin Nordholts <martin.nordholts@codetale.se>
2026-03-26 06:17:44 +0100

    wip  2026-03-26T06:17:44+01:00

--- tests/ui/borrowck/tests/ui/borrowck/unused-param-not-metioned-in-async-static-error.rs
+++ tests/ui/borrowck/tests/ui/borrowck/unused-param-not-metioned-in-async-static-error.rs
@@ -6,7 +6,8 @@ pub fn takes_static<T: 'static>(_: T) {}
 
 // `bar_static` is unused and should not be mentioned in the error message.
 fn foo<'foo>(foo: &'foo u8, bar_static: &'static u16) {
-    takes_static(move || foo ); //~ ERROR: borrowed data escapes outside of function
+    let the_closure = move || foo;
+    takes_static(the_closure); //~ ERROR: borrowed data escapes outside of function
 }
 
 fn main() {}
