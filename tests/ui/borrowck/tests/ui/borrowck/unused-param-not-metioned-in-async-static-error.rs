715770ccc02e1512925dda55b2194d6b2b10ee3e
Martin Nordholts <martin.nordholts@codetale.se>
2026-03-26 06:13:51 +0100

    wip  2026-03-26T06:13:51+01:00

--- tests/ui/borrowck/tests/ui/borrowck/unused-param-not-metioned-in-async-static-error.rs
+++ tests/ui/borrowck/tests/ui/borrowck/unused-param-not-metioned-in-async-static-error.rs
@@ -1,4 +1,4 @@
-//! Partly a regression test for <https://github.com/rust-lang/rust/issues/113121>.
+//! Regression test for <https://github.com/rust-lang/rust/issues/113121>.
 
 //@ edition:2018
 
@@ -6,8 +6,7 @@ pub fn takes_static<T: 'static>(_: T) {}
 
 // `bar_static` is unused and should not be mentioned in the error message.
 fn foo<'foo>(foo: &'foo u8, bar_static: &'static u16) {
-    takes_static(move || std::hint::black_box(foo) );
-    //~^ ERROR: borrowed data escapes outside of function
+    takes_static(|| foo ); //~ ERROR: borrowed data escapes outside of function
 }
 
 fn main() {}
