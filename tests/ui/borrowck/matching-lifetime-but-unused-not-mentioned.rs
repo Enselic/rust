dfdd35b4acd433a6040143d155b673d3f401b17f
Martin Nordholts <martin.nordholts@codetale.se>
2026-03-30 18:38:13 +0200

    wip  2026-03-30T18:38:13+02:00

--- tests/ui/borrowck/matching-lifetime-but-unused-not-mentioned.rs
+++ tests/ui/borrowck/matching-lifetime-but-unused-not-mentioned.rs
@@ -4,13 +4,16 @@
 
 #![allow(unused_variables)]
 
-fn takes_static<T: 'static>(_: T) {}
+fn consume<T: 'static>(_: T) {}
 
-// `arg_static` and `upvar_static` are unused and should not be mentioned in the error message.
-fn foo<'a>(foo: &'a u8, arg_static: &'static u16) {
-    let upvar_static: &'static u16 = &42;
+fn foo<'a>(
+    foo: &'a u8,
+    bar: &'static u16, // Unused in closure. Must not be in error.
+) {
+    let baz: &'static u32 = &42; // Unused in closure. Must not be in error.
+
     let the_closure = move || foo;
-    takes_static(the_closure); //~ ERROR: borrowed data escapes outside of function
+    consume(the_closure); //~ ERROR: borrowed data escapes outside of function
 }
 
 fn main() {}
