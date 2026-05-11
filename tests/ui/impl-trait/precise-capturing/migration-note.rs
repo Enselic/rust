8a033e136344d21bb0b1449733fe58036b711410
Martin Nordholts <martin.nordholts@codetale.se>
2026-05-11 21:25:19 +0200

    tests/ui/impl-trait/precise-capturing/migration-note.rs

--- tests/ui/impl-trait/precise-capturing/migration-note.rs
+++ tests/ui/impl-trait/precise-capturing/migration-note.rs
@@ -2,7 +2,7 @@
 
 use std::fmt::Display;
 
-fn display_len<T>(x: &Vec<T>) -> impl Display {
+fn display_len<T>(x: &Vec<T>) -> impl Display { //~ NOTE function defined here
     //~^ NOTE in this expansion of desugaring of `impl Trait`
     //~| NOTE in this expansion of desugaring of `impl Trait`
     //~| NOTE in this expansion of desugaring of `impl Trait`
