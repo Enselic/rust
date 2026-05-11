23f2a22d1d1b3e5c7127f4004a321a6f9a983388
Martin Nordholts <martin.nordholts@codetale.se>
2026-05-11 21:14:02 +0200

    fixup! borrowck: For "outlives" diagnostics, add "call argument" `fn` span

--- compiler/rustc_borrowck/src/diagnostics/explain_borrow.rs
+++ compiler/rustc_borrowck/src/diagnostics/explain_borrow.rs
@@ -8,8 +8,8 @@ use rustc_hir::intravisit::Visitor;
 use rustc_infer::infer::NllRegionVariableOrigin;
 use rustc_middle::middle::resolve_bound_vars::ObjectLifetimeDefault;
 use rustc_middle::mir::{
-    Body, CallSource, CastKind, ConstraintCategory, FakeReadCause, Local, LocalInfo, Location,
-    Operand, Place, Rvalue, Statement, StatementKind, TerminatorKind,
+    AnnotationSource, Body, CallSource, CastKind, ConstraintCategory, FakeReadCause, Local,
+    LocalInfo, Location, Operand, Place, Rvalue, Statement, StatementKind, TerminatorKind,
 };
 use rustc_middle::ty::adjustment::PointerCoercion;
 use rustc_middle::ty::{self, RegionVid, Ty, TyCtxt};
@@ -427,17 +427,33 @@ impl<'tcx> BorrowExplanation<'tcx> {
                 if !preds.is_empty() {
                     let s = if preds.len() == 1 { "" } else { "s" };
                     err.span_note(
-                        preds,
+                        preds.clone(),
                         format!(
                             "requirement{s} that the value outlives `{region_name}` introduced here"
                         ),
                     );
-                } else {
-                    if let ConstraintCategory::CallArgument(Some(fn_)) = category
-                        && let ty::FnDef(fn_def_id, _) = fn_.kind()
-                    {
+                }
+
+                // Check if ConstraintCategory::TypeAnnotation is part of the path:
+                let has_type_annotation = path.iter().any(|constraint| {
+                    matches!(
+                        constraint.category,
+                        ConstraintCategory::TypeAnnotation(AnnotationSource::GenericArg)
+                    )
+                });
+                if let ConstraintCategory::CallArgument(Some(fn_)) = category
+                    && let ty::FnDef(fn_def_id, _) = fn_.kind()
+                    && !has_type_annotation
+                {
+                    let fn_span = tcx.def_span(*fn_def_id);
+                    // If the the constraint comes from a call argument, show
+                    // the function definition as additional context. However,
+                    // if `preds` already overlaps with the function definition,
+                    // then it is very likely that the relevant context is
+                    // already shown, so we can skip showing it again.
+                    if !preds.iter().any(|pred| pred.overlaps(fn_span)) {
                         err.span_note(
-                            tcx.def_span(*fn_def_id),
+                            fn_span,
                             format!("{} defined here", tcx.def_descr(*fn_def_id)),
                         );
                     }
