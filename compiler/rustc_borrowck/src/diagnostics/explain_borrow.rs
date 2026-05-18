3ec2054d8995e31a9beb7a1647f4876b3afd4016
Martin Nordholts <martin.nordholts@codetale.se>
2026-05-18 13:56:51 +0200

    foo

--- compiler/rustc_borrowck/src/diagnostics/explain_borrow.rs
+++ compiler/rustc_borrowck/src/diagnostics/explain_borrow.rs
@@ -580,7 +580,12 @@ impl<'tcx> BorrowExplanation<'tcx> {
             .iter()
             .any(|span_label| span_label.label.is_some() && span_label.span.overlaps(fn_span));
 
-        if !has_overlapping_label {
+        let has_overlapping_subdiag = err
+            .children
+            .iter()
+            .any(|child| child.span.primary_spans().iter().any(|s| s.overlaps(fn_span)));
+
+        if !has_overlapping_label && !has_overlapping_subdiag {
             err.span_note(fn_span, format!("{} defined here", tcx.def_descr(*fn_def_id)));
         }
     }
