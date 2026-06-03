3544a7cdd632941a558137d88b04700144a25ebd
Martin Nordholts <martin.nordholts@codetale.se>
2026-06-03 06:38:53 +0200

    bring-back

--- compiler/rustc_borrowck/src/type_check/mod.rs
+++ compiler/rustc_borrowck/src/type_check/mod.rs
@@ -2043,7 +2043,7 @@ impl<'a, 'tcx> TypeChecker<'a, 'tcx> {
             let op_arg_ty = self.normalize(ty::Unnormalized::new_wip(op_arg_ty), term_location);
             let category = if call_source.from_hir_call() {
                 ConstraintCategory::CallArgument(Some(CallArgumentConstraintCategory::new(
-                    func_ty, op_arg.span,
+                    self.infcx.tcx.erase_and_anonymize_regions(func_ty), op_arg.span,
                 )))
             } else {
                 ConstraintCategory::Boring
